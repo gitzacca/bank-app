dbe8273d3af25d50c2b651da6a6da9551b19092b
Bruno Zaccaria Marques <zaccabruno@gmail.com>
2018-07-23 10:15:24 -0300

    Added run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+
+set -e
+
+cd `dirname $0`
+r=`pwd`
+echo $r
+
+# Accounts
+cd $r/accounts
+echo "Starting accounts service..."
+./gradlew bootRun &
+
+# Transactions
+echo "Starting Tranasctions Service..."
+cd $r/transactions
+./gradlew bootRun &
